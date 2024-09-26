8c53b9884f3a760805e8cd5db7279f26e03406d0
Vishvajeet shukla <djritikshukla@gmail.com>
2024-09-26 12:53:14 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      // Mark /src/main.jsx as an external module to prevent Rollup from bundling it
+      external: ['/src/main.jsx'],
+    },
+  },
+});
